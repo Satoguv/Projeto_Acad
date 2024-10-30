f5899a39e20d3cb04df6dff7f92f13d4f52b4f09
Satoguv <guscafer@gmail.com>
2024-10-30 11:49:18 -0300

    Veja aí

---
+++ acad.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class acad {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int opcao;
+        String sair;
+    }
+}

47502a4ffd93bc28c4051d23034c2b19d9e8fa99
Satoguv <guscafer@gmail.com>
2024-10-30 12:08:56 -0300

    Mais alterações

--- acad.java
+++ acad.java
@@ -5,7 +5,16 @@ public class acad {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int opcao;
-        String sair;
+        int opcao = scan.nextInt();
+        String status = scan.nextLine();
+
+        menu:
+        while (true) {
+            System.out.println("");
+            
+        }
+
+
+        
     }
 }
